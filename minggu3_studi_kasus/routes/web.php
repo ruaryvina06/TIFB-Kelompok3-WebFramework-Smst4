a2f4a855ff1290124e221997eaa5885893628b48
nurillahilwafi <nurilwafi17@gmail.com>
2022-03-08 08:40:54 +0700

    minggu3 studi kasus

---
+++ minggu3_studi_kasus/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/home', function () {
+    return redirect('home');
+});
+
+// Route::view('/home', 'home');
+// Route::view('/login', 'login');
